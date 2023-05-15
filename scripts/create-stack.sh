c883c55157f89bb447fc31f023394976ba6ba2ab
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-05-15 07:44:16 -0400

    added create stack script

---
+++ scripts/create-stack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+USAGE='USAGE: ./create-stack.sh <STACK_NAME>'
+STACK_NAME=${1?$USAGE}
+
+aws cloudformation create-stack \
+  --stack-name $STACK_NAME \
+  --template-body file://stacks/$STACK_NAME.yaml \
+  --profile root
+
+STACK_STATUS='init'
+
+while [ $STACK_STATUS != 'CREATE_COMPLETE' ]; do
+  STACK_STATUS=$(aws cloudformation describe-stacks \
+    --profile root \
+    --stack-name $STACK_NAME \
+    | jq -r '.Stacks[0].StackStatus')
+  echo "STACK_STATUS: $STACK_STATUS"
+  sleep 2
+done
