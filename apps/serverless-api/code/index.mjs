2a08344d1eca74f5affe9617d962dd28b952959c
edwmurph <20996513+edwmurph@users.noreply.github.com>
2023-05-20 13:42:57 -0400

    serverless api tweaks

--- apps/serverless-api/code/index.mjs
+++ apps/serverless-api/code/index.mjs
@@ -1,7 +1,12 @@
+const date1 = new Date().toISOString();
+
 export const handler = async ( event, context ) => {
   console.log({ event, context });
   return {
     'statusCode': 200,
-    'body': JSON.stringify({ message: 'hello world' })
+    'body': JSON.stringify({ message: 'hello world',
+      date1,
+      date2: new Date().toISOString()
+    })
   };
 };
